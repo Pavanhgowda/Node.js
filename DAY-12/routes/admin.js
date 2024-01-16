29b20fae5622381a4ddf0a311ff1e455e336f51a
Pavan <pavanh933@gmail.com>
2024-01-16 10:34:34 +0530

    Add: Working on Shopping app using ejs

---
+++ DAY-12/routes/admin.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/shop');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products');
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
+
+
+
+module.exports = router;
---
+++ DAY-12/routes/shop.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+router.get('/products',);
+
+router.get('/cart',);
+
+router.get('/checkout',);
+
+router.get('')
+
+module.exports = router;
